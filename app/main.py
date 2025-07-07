4dc951e963a09de7d7db77d9368019575538198e
Abdullah <abdulhaseeb9274@gmail.com>
2025-07-07 20:49:35 +0500

    /db-check api and updated (#2)
    Approve.

--- app/main.py
+++ app/main.py
@@ -1,8 +1,11 @@
 from fastapi import FastAPI
+from app.api.routes import test
 
-app = FastAPI()
-
+# app = FastAPI()
+app = FastAPI(swagger_ui_parameters={"syntaxHighlight": {"theme": "obsidian"}})
 
 @app.get("/")
 async def root():
     return {"message": "Hello, Universe! from e-mahhir!"}
+
+app.include_router(test.router)
